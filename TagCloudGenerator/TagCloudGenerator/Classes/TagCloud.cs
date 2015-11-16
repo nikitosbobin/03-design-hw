07d5be51e5fc7c43deb15346be066132684c8f30
Nikita <nikitosbobin@gmail.com>
2015-11-16 22:27:24 +0500

    Initial. Начал строить архитектуру программы.

---
+++ TagCloudGenerator/TagCloudGenerator/Classes/TagCloud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagCloudGenerator.Interfaces;
+
+namespace TagCloudGenerator.Classes
+{
+    class TagCloud
+    {
+        public bool GenerateImage(IAlgorithm formAlg, IImageEncoder encoder)
+        {
+            return false;
+        }
+    }
+}
